2e16bb9fce4b5581a4f6a5c9bba80118ff618900
Oskariņš <oskarsplotnieks@gmail.com>
2025-04-08 20:27:49 +0300

    Viss bumbas

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    gads = datetime.datetime.now().year
+    return render_template("index.html", gads=gads)
+
+@app.route('/klasiskas')
+def klasiskas():
+    return render_template('/Enges/klasiskas.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
