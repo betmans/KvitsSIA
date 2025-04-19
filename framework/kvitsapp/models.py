2c438d473a0ffe824f1c0d5348ffbc1815b54a1f
Oskariņš <oskarsplotnieks@gmail.com>
2025-04-19 22:40:22 +0300

    Nomainiju uz django un uzlaboju lietas

---
+++ framework/kvitsapp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# class Enge(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class aizbidni_krampisi(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class sledzenes(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class durvju_rokturi(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class cilindri_aizgriezni(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class mebelu_furnitura(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class aksesuari(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+# class stiprinajumi(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField()
+
+#     def __str__(self):
+#         return self.name
