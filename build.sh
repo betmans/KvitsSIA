64f74f1639f9fb2563754283fe4d83509326dc48
betmans <oskarsplotnieks@gmail.com>
2025-05-07 13:47:44 +0300

    Datubazei

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# Populate products (TEMPORARY - REMOVE AFTER FIRST SUCCESSFUL IMPORT)
+echo "Attempting to import products..."
+python manage.py import_products 
+echo "Product import command finished."
