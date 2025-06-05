2226ae5fb133ed3e9550983add9e0b87b0011d0b
Oskariņš <oskarsplotnieks@gmail.com>
2025-06-05 10:25:00 +0300

    +gunicorn build

--- build.sh
+++ build.sh
@@ -10,5 +10,10 @@ python manage.py migrate
 
 # Populate products (TEMPORARY - REMOVE AFTER FIRST SUCCESSFUL IMPORT)
 # echo "Attempting to import products..."
-# python manage.py import_products 
+# python manage.py import_products
 # echo "Product import command finished."
+
+# Start Gunicorn
+# Ensure Gunicorn is in your requirements.txt
+# The PORT environment variable is automatically set by Cloud Run.
+exec gunicorn kvits.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --threads 4 --worker-tmp-dir /dev/shm
